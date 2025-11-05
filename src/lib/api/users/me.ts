8bfe47fc023c65a35bbee7a2e0459042f48d4fae
jungsikJeong <wndtlr1024@gmail.com>
2025-11-05 18:22:37 +0900

    refactor: 주석 제거

--- src/lib/api/users/me.ts
+++ src/lib/api/users/me.ts
@@ -6,7 +6,6 @@ import { UserDetail } from "domains/auth/types"
 import { cookies, headers } from "next/headers"
 import { cache } from "react"
 
-// todo: 네트워크 에러 처리하고 클라이언트 토스트 컴포넌트에 보내는거 추가
 export const fetchCurrentUser = cache(async (): Promise<UserDetail | null> => {
   const cookieStore = await cookies()
   const headersList = await headers()
