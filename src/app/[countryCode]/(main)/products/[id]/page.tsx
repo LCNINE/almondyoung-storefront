037f14b7f101642d41169221a573e20c070bbfcc
jungsikjeong <wndtlr1024@gmail.com>
2026-01-21 10:40:03 +0900

    refactor: 주석 로그 다시 되돌리고, wishlist catch null추가해서 로그인안한사용자 에러페이지로가서 로그인페이지로 리다이렉트안되도록 처리해줌

--- src/app/[countryCode]/(main)/products/[id]/page.tsx
+++ src/app/[countryCode]/(main)/products/[id]/page.tsx
@@ -162,7 +162,9 @@ export default async function Page({
   let product: ProductDetail | null = null
   let error: string | null = null
   const user: UserDetail | null = await fetchMe().catch(() => null)
-  const wishlist: WishlistItem | null = await getWishlistByProductId(id)
+  const wishlist: WishlistItem | null = await getWishlistByProductId(id).catch(
+    () => null
+  )
   const region = await getRegion(countryCode)
   const salesChannelId = await getDefaultSalesChannelId()
 
@@ -174,21 +176,21 @@ export default async function Page({
     )
     const sampleVariant = medusaProduct?.variants?.[0]
     if (sampleVariant) {
-      // console.log("[PRICE DEBUG] Store API Response", {
-      //   productId: medusaProduct?.id,
-      //   regionId: region?.id,
-      //   regionCurrencyCode: region?.currency_code,
-      //   salesChannelId,
-      //   variantId: sampleVariant.id,
-      //   calculated_price: sampleVariant.calculated_price,
-      // })
-      // console.log("[REGION DEBUG]", {
-      //   regionId: region?.id,
-      //   regionName: region?.name,
-      //   currencyCode: region?.currency_code,
-      //   countries: region?.countries?.map((c) => c.iso_2),
-      //   _fullRegion: JSON.stringify(region, null, 2),
-      // })
+      console.log("[PRICE DEBUG] Store API Response", {
+        productId: medusaProduct?.id,
+        regionId: region?.id,
+        regionCurrencyCode: region?.currency_code,
+        salesChannelId,
+        variantId: sampleVariant.id,
+        calculated_price: sampleVariant.calculated_price,
+      })
+      console.log("[REGION DEBUG]", {
+        regionId: region?.id,
+        regionName: region?.name,
+        currencyCode: region?.currency_code,
+        countries: region?.countries?.map((c) => c.iso_2),
+        _fullRegion: JSON.stringify(region, null, 2),
+      })
     }
     let pimDescriptionHtml: string | undefined
     let pimMasterId: string | undefined
