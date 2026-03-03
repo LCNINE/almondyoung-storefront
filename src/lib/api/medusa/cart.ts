9a3b366d5bdff4c97365f302b09ed1ce6b9ad10d
Pear-HyunJI <ponyo3764@gmail.com>
2026-03-03 14:46:26 +0900

    .

--- src/lib/api/medusa/cart.ts
+++ src/lib/api/medusa/cart.ts
@@ -518,51 +518,51 @@ export async function applyPromotions(codes: string[]) {
     .catch(medusaError)
 }
 
-export async function applyGiftCard(code: string) {
-  //   const cartId = getCartId()
-  //   if (!cartId) return "No cartId cookie found"
-  //   try {
-  //     await updateCart(cartId, { gift_cards: [{ code }] }).then(() => {
-  //       revalidateTag("cart")
-  //     })
-  //   } catch (error: any) {
-  //     throw error
-  //   }
-}
+// export async function applyGiftCard(code: string) {
+//   const cartId = getCartId()
+//   if (!cartId) return "No cartId cookie found"
+//   try {
+//     await updateCart(cartId, { gift_cards: [{ code }] }).then(() => {
+//       revalidateTag("cart")
+//     })
+//   } catch (error: any) {
+//     throw error
+//   }
+// }
 
-export async function removeDiscount(code: string) {
-  // const cartId = getCartId()
-  // if (!cartId) return "No cartId cookie found"
-  // try {
-  //   await deleteDiscount(cartId, code)
-  //   revalidateTag("cart")
-  // } catch (error: any) {
-  //   throw error
-  // }
-}
+// export async function removeDiscount(code: string) {
+// const cartId = getCartId()
+// if (!cartId) return "No cartId cookie found"
+// try {
+//   await deleteDiscount(cartId, code)
+//   revalidateTag("cart")
+// } catch (error: any) {
+//   throw error
+// }
+// }
 
-export async function removeGiftCard(
-  codeToRemove: string,
-  giftCards: any[]
-  // giftCards: GiftCard[]
-) {
-  //   const cartId = getCartId()
-  //   if (!cartId) return "No cartId cookie found"
-  //   try {
-  //     await updateCart(cartId, {
-  //       gift_cards: [...giftCards]
-  //         .filter((gc) => gc.code !== codeToRemove)
-  //         .map((gc) => ({ code: gc.code })),
-  //     }).then(() => {
-  //       revalidateTag("cart")
-  //     })
-  //   } catch (error: any) {
-  //     throw error
-  //   }
-}
+// export async function removeGiftCard(
+//   codeToRemove: string,
+//   giftCards: any[]
+// giftCards: GiftCard[]
+//) {
+//   const cartId = getCartId()
+//   if (!cartId) return "No cartId cookie found"
+//   try {
+//     await updateCart(cartId, {
+//       gift_cards: [...giftCards]
+//         .filter((gc) => gc.code !== codeToRemove)
+//         .map((gc) => ({ code: gc.code })),
+//     }).then(() => {
+//       revalidateTag("cart")
+//     })
+//   } catch (error: any) {
+//     throw error
+//   }
+//}
 
 export async function submitPromotionForm(
-  currentState: unknown,
+  // currentState: unknown,
   formData: FormData
 ) {
   const code = formData.get("code") as string
@@ -574,7 +574,7 @@ export async function submitPromotionForm(
 }
 
 // TODO: Pass a POJO instead of a form entity here
-export async function setAddresses(currentState: unknown, formData: FormData) {
+export async function setAddresses(formData: FormData) {
   try {
     if (!formData) {
       throw new Error("No form data found when setting addresses")
